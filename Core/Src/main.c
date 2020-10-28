5ce2e39afc8d8b9f414dbb24087f6c7149153ffe
Mantas A <mantas.ambraziunas@gmail.com>
2020-10-28 17:52:57 +0200

    Prideti I2C sensoriai
    Integruotas Aistes jutikliu kodas

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -24,7 +24,6 @@
 /* USER CODE BEGIN Includes */
 #include <stdlib.h>
 #include <string.h>
-
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -42,8 +41,11 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
 UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart6;
+DMA_HandleTypeDef hdma_usart1_tx;
 DMA_HandleTypeDef hdma_usart6_rx;
 
 /* USER CODE BEGIN PV */
@@ -55,6 +57,50 @@ struct {
 
 uint8_t mGPS_UART_Buffer[1000];
 
+#define acc_gyr_address 0xD7
+#define CTRL3_C 0x12 // control register for incrementing
+#define acc_on_reg 0x10 // acc mode (turns on acc)
+#define CTRL6_C 0x15 // control register for acc modes
+#define OUTX_L_G 0x22 // first register of gyr reading
+uint8_t reg_increment[1] = {0x04}; // register increment
+uint8_t acc_gyr_on_values[2] = {0x50, 0x50}; // turn on acc and gyr
+uint8_t acc_gyr_modes[2] = {0x10,0x80}; // low/normal modes for acc and gyr
+uint8_t data_acc_gyr[12]; // data received from acc and gyr sensors
+int16_t unpacked[6], acc_xx, acc_yy, acc_zz, gyr_xx, gyr_yy, gyr_zz;
+
+//temperature and humidity variables
+/*
+#define temp_hum_address 0xBC
+#define writing_th 0xBE
+#define reading_th 0xBF
+uint8_t hum_temp_on[2] ={0x20, 0x83}; //turn on sensor + 12.5Hz, NOT one-shot
+uint8_t HUMIDITY_OUT_L[1] = {0x28};
+uint8_t data_temp_hum[4];
+uint8_t who_am_I_reg[1] = {0x0F};
+uint8_t whoami[1];
+int16_t unpacked_temp_hum[2], temp, hum;
+*/
+//magnetometer variables
+#define magn_address 0x33 // magnetometer sensor adress
+#define reading_magn 0x3D // adress to read from magn sensor registers
+#define writing_magn 0x3C // address to write to magn sensor registers
+#define magn_on_mode 0x60 // control register to turn on magnetometer
+#define OUTX_L_REG_M 0x68|0x80 // magnetometer XX YY ZZ
+uint8_t magn_on[1] = {0x1C}; // continuous/single measurement mode
+uint8_t data_magn[6];
+int16_t unpacked_magn[3], magn_x, magn_y, magn_z;
+
+//ACC
+#define writing_acc2 0x32 // address to write to acc registers
+#define reading_acc2 0x33 // address to read from acc registers
+#define CTRL_REG1_A 0x20 // modes register
+#define reg_out_acc2 0x28 // first register to output data
+uint8_t reg[1] = {0x57}; // normal mode on
+uint8_t data_acc2[12];
+int32_t unpacked_acc2[3];
+
+uint16_t checkout;
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -63,6 +109,7 @@ static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_USART6_UART_Init(void);
 static void MX_USART1_UART_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -109,6 +156,7 @@ int main(void)
   MX_DMA_Init();
   MX_USART6_UART_Init();
   MX_USART1_UART_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -127,7 +175,7 @@ int main(void)
 
 		  // Surasti nuskaitytos zinutes ilgi
 		  ilgis=strlen((char*)mGPS_UART_Buffer);
-//  		  HAL_UART_Transmit(&huart6, mGPS_UART_Buffer, ilgis, 10);	// Pilnas paketas
+		  HAL_UART_Transmit_DMA(&huart6, mGPS_UART_Buffer, ilgis);	// Pilnas paketas
 
 		  // Surasti reikiamos eilutes pradzia ir pabaiga
 		  beginning = getNewlineIndex(mGPS_UART_Buffer, ilgis, 1);
@@ -136,12 +184,68 @@ int main(void)
 		  ilgis=end-beginning;	// Surinktos eilutes ilgis
 
 		  // Persiusti masyva per UART
-		  HAL_UART_Transmit(&huart6, &mGPS_UART_Buffer[beginning], ilgis, 10);
+		  HAL_UART_Transmit_DMA(&huart6, &mGPS_UART_Buffer[beginning], ilgis);
 
 		  // Nunulinti masyva
 		  memset(mGPS_UART_Buffer, 0, sizeof mGPS_UART_Buffer);
 	  }
 
+	  //________________ READING DATA FROM ACCELEROMETER AND GYROSCOPE
+	  // Turn on acc and reg, auto increment, set low/normal modes for acc and gyr, read data
+		  HAL_I2C_Mem_Write(&hi2c1, acc_gyr_address, acc_on_reg, 1, acc_gyr_on_values, 2, 10);
+		  HAL_I2C_Mem_Write(&hi2c1, acc_gyr_address, CTRL3_C, 1, reg_increment, 1, 10);
+		  HAL_I2C_Mem_Write(&hi2c1, acc_gyr_address, CTRL6_C, 1, acc_gyr_modes, 2, 10);
+		  HAL_I2C_Mem_Read(&hi2c1, acc_gyr_address, OUTX_L_G, 1, data_acc_gyr, 12, 10);
+		  //____________ UNPACKING DATA FROM ACCELEROMETER AND GYROSCOPE
+		  int k = 0;
+		  for (int i = 0; i < 6; i++) { unpacked[i] = (data_acc_gyr[k+1] << 8) | data_acc_gyr[k]; k=k+2; }
+		  gyr_xx=unpacked[0]*8.75/1000;
+		  gyr_yy=unpacked[1]*8.75/1000;
+		  gyr_zz=unpacked[2]*8.75/1000;
+		  acc_xx=unpacked[3]*0.061;
+		  acc_yy=unpacked[4]*0.061;
+		  acc_zz=unpacked[5]*0.061;
+
+		  //____________________READING DATA FROM TEMPERATURE AND HUMIDITY SENSOR
+
+		  /*
+		  HAL_I2C_Master_Transmit(&hi2c1, writing_th, who_am_I_reg, 1, 10);
+		  HAL_Delay(30);
+		  HAL_I2C_Master_Receive(&hi2c1, reading_th, whoami, 1, 10);
+		  HAL_Delay(30);
+		  HAL_I2C_Master_Transmit(&hi2c1, writing_th, hum_temp_on, 2, 10);
+		  HAL_Delay(30);
+		  HAL_I2C_Master_Transmit(&hi2c1, writing_th, HUMIDITY_OUT_L, 1, 10);
+		  HAL_Delay(30);
+		  HAL_I2C_Master_Receive(&hi2c1, reading_th, data_temp_hum, 4, 10);
+		  HAL_Delay(03);
+		  //___________________UNPACKING DATA FROM TEMPERATURE AND HUMIDITY SENSOR
+		  k=0;
+		  for (int i = 0; i < 2; i++) { unpacked_temp_hum[i] = (data_temp_hum[k+1] << 8) | data_temp_hum[k]; k=k+2; }
+		  hum=unpacked_temp_hum[0];
+		  temp=unpacked_temp_hum[1];
+		  */
+
+		  //___________________READING DATA FROM AKSELEROMETER AND MANGETOMETER
+
+		  HAL_I2C_Mem_Write(&hi2c1, writing_magn, magn_on_mode, 1, magn_on, 1, 10);
+		  HAL_I2C_Mem_Read(&hi2c1, reading_magn, OUTX_L_REG_M, 1, data_magn, 6, 10);
+
+		  //___________________UNPACKING DATA, MAGNETOMETER
+		  k=0;
+		  for (int i = 0; i < 3; i++) { unpacked_magn[i] = (data_magn[k+1] << 8) | data_magn[k]; k=k+2; }
+		  magn_x=unpacked_magn[0];
+		  magn_y=unpacked_magn[1];
+		  magn_z=unpacked_magn[2];
+
+		  //___________________READING DATA FROM ACCELEROMETER
+		  HAL_I2C_Mem_Write(&hi2c1, writing_acc2, CTRL_REG1_A, 1, reg, 1, 10);
+		  HAL_I2C_Mem_Read(&hi2c1, reading_acc2, reg_out_acc2, 1, data_acc2, 12, 10);
+
+		  // twos complement left-justified xxxx xxxx xx00 0000
+		  unpacked_acc2[0] = ((data_acc2[3] & 0xC0) << 18) | (data_acc2[2] << 10 ) | ((data_acc2[1] & 0xC0) << 8) | data_acc2[0];
+		  unpacked_acc2[1] = ((data_acc2[7] & 0xC0) << 18) | (data_acc2[6] << 10 ) | ((data_acc2[5] & 0xC0) << 8) | data_acc2[4];
+		  unpacked_acc2[2] = ((data_acc2[11] & 0xC0) << 18) | (data_acc2[10] << 10 ) | ((data_acc2[9] & 0xC0) << 8) | data_acc2[8];
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -191,6 +295,40 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 400000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
 }
 
 /**
@@ -272,6 +410,9 @@ static void MX_DMA_Init(void)
   /* DMA2_Stream1_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA2_Stream1_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(DMA2_Stream1_IRQn);
+  /* DMA2_Stream7_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA2_Stream7_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA2_Stream7_IRQn);
 
 }
 
